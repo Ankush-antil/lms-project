5276d8f0f31578a70220081eead2ae27085676a8
Ankush <ankushantil7036@gmail.com>
2026-07-20 19:28:43 +0530

    fix(db): add family 4 and connectTimeoutMS options for Mongoose connection

--- server/config/db.js
+++ server/config/db.js
@@ -7,6 +7,9 @@ const connectDB = async () => {
 
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             serverSelectionTimeoutMS: 10000,
+            connectTimeoutMS: 10000,
+            socketTimeoutMS: 45000,
+            family: 4
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {

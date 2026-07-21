ba2b97977ca3287e5168b9c0bdd158249af6ffa3
Ankush <ankushantil7036@gmail.com>
2026-07-21 13:07:38 +0530

    tools and remove web caling

--- LMSMobileApp/src/config/api.js
+++ LMSMobileApp/src/config/api.js
@@ -1,7 +1,4 @@
-// Production Server - DigitalOcean (Temporarily disabled due to DigitalOcean MongoDBAtlas IP whitelist timeout)
-// export const BASE_URL = 'https://www.digitalstudyacademy.com';
-
-// Local Working Development Server
-export const BASE_URL = 'http://127.0.0.1:5000';
+// Production Server Domain
+export const BASE_URL = 'https://www.digitalstudyacademy.com';
 
 export const API_URL = `${BASE_URL}/api`;
